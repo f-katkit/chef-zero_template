36dbca3bf60873f9ca3a6cb903c6361f2b183a0a
katkit <katkitgit@gmail.com>
2016-10-29 14:07:23 +0900

    first init

---
+++ provisioning/init.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# generate onetime sshpass
+# SSHPASS=$(sudo ifconfig | head -1 |awk '{print $5}' |openssl sha1|awk '{print $2}'
+
+# install utility application.
+sudo apt-get update
+sudo apt-get install -yq wget git ruby curl vim openssl openssh-server
+
+# enable root login
+sudo sed -i -e "s/PermitRootLogin = .*/PermitRootLogin = without-password/g" /etc/ssh/sshd_config
+sudo ssh-keygen -f /root/.ssh/id_rsa -t rsa -N ""
+sudo cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
+sudo cat << EOT >> /root/.ssh/config
+Host 127.0.0.1
+  HostName 127.0.0.1
+  User root
+  Port 22
+  IdentityFile /root/.ssh/id_rsa
+  StrictHostKeyChecking no
+  UserKnownHostsFile=/dev/null
+EOT
+
+sudo chmod 600 /root/.ssh/*
+sudo chmod 700 /root/.ssh
+sudo service ssh restart
+
+# install chef
+wget https://packages.chef.io/stable/ubuntu/12.04/chefdk_0.19.6-1_amd64.deb
+dpkg -i chefdk_0.19.6-1_amd64.deb
+chef gem install knife-zero
+
+# git repository clone
+# git clone *****
+
+# install bootstrap
+ knife zero bootstrap localhost -x root --node-name localhost
